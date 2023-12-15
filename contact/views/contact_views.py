77b37b432355118d6283af038da993a3988b63ef
thales <thalesa202@gmail.com>
2023-12-15 19:33:44 -0300

    upgrade

---
+++ contact/views/contact_views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from contact.models import Contact
+
+def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id') [0:10]
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+        
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+    
+    
+def contact(request, contact_id):
+    #single_contact = Contact.objects.filter(pk=contact_id).first()
+    
+    single_contact =get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+        
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
