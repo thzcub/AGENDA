3a31659d6aac07bd60e9caa8affe499cf6bad311
thales <thalesa202@gmail.com>
2023-12-21 01:22:15 -0300

    up

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
-
+from django.db.models import Q
 from contact.models import Contact
 
 def index(request):
@@ -16,6 +16,34 @@ def index(request):
         context
     )
     
+    
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+        
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value':search_value,
+    }
+        
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
     
 def contact(request, contact_id):
     #single_contact = Contact.objects.filter(pk=contact_id).first()
